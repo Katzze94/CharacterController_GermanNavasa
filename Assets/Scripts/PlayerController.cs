b2fad4766972607fb2f57a4b3767333946c4d066
Katzze94 <germannavasa2003@gmail.com>
2024-10-25 10:58:07 +0200

    Commit_Personaje
    Movimiento y cámara

---
+++ Assets/Scripts/PlayerController.cs
@@ -0,0 +1,105 @@
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using Unity.VisualScripting;
+using UnityEngine;
+using UnityEngine.TextCore.Text;
+
+public class PlayerController : MonoBehaviour
+{
+
+    private CharacterController _controller;
+
+    private float _horizontal;
+
+    private float _vertical;
+
+    [SerializeField] private float _movementSpeed = 5;
+
+    private float _turnSmoothVelocity;
+    [SerializeField] private float _turnSmoothTime = 0.5f;
+
+
+//----------------Gravedad--------------------------
+    [SerializeField] private float _gravity = -9.81f;
+    [SerializeField] private Vector3 _playerGravity;
+
+
+//----------------Ground Sensor -------------------
+    [SerializeField] Transform _sensorPosition;
+
+    [SerializeField] float _sensorRadius = 0.5f;
+
+    [SerializeField] LayerMask _groundLayer;
+
+
+
+
+    void Awake()
+    {
+        _controller = GetComponent<CharacterController>();
+    }
+   
+   
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        _horizontal = Input.GetAxis("Horizontal");
+        _vertical = Input.GetAxis("Vertical");
+       
+        Movement();
+       
+        Gravity();
+    }
+
+    void Movement()
+    {
+        Vector3 direction = new Vector3(_horizontal, 0, _vertical);
+
+        if(direction != Vector3.zero)
+        {
+            float targetAngle = Mathf.Atan2(direction.x, direction.z) * Mathf.Rad2Deg;
+
+            float smoothAngle = Mathf.SmoothDampAngle(transform.eulerAngles.y, targetAngle, ref _turnSmoothVelocity, _turnSmoothTime);
+
+           transform.rotation = Quaternion.Euler(0, smoothAngle, 0);
+
+            _controller.Move(direction * _movementSpeed * Time.deltaTime);
+        }
+        
+    }
+
+    void Gravity()
+    {
+       if(!IsGrounded())
+       {
+        _playerGravity.y += _gravity *Time.deltaTime;
+       }
+        else if(IsGrounded() && _playerGravity.y < 0)
+        {
+            _playerGravity.y = -1;
+        }
+
+        _controller.Move(_playerGravity * Time.deltaTime);
+        
+    }
+
+    bool IsGrounded()
+    {
+        return Physics.CheckSphere(_sensorPosition.position, _sensorRadius, _groundLayer);
+    }
+
+    void OnDrawGizmos()
+    {
+        Gizmos.color = Color.green;
+        Gizmos.DrawWireSphere(_sensorPosition.position, _sensorRadius);
+    }
+}
+
+
