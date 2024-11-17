8494d2e604990cdb13d24d947ca8919ed7ffcd08
Katzze94 <germannavasa2003@gmail.com>
2024-11-17 14:50:14 +0100

    Commit_DiaYNoche

---
+++ Assets/Scripts/CicloDiaYNoche.cs
@@ -0,0 +1,22 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CicloDiaYNoche : MonoBehaviour
+{
+    
+    public int rotationScale = 10;
+
+   
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        transform.Rotate(rotationScale * Time.deltaTime, 0, 0);
+        
+    }
+}
